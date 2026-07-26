bad54dd23e8f2ee94573f39224d2b270d8377e7b
abhijeetvilayate <vilayateabhijeet1073@gmail.com>
2026-07-27 03:18:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // <-- Changed to HashRouter
 import Header from "./Main/Header";
 
 // Importing Page Components from src/Pages/
